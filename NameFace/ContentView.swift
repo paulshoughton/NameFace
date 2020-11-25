1b0a04eddd3f2b3f7694ea6137408b0012649740
Paul Houghton <paul.houghton@gmail.com>
2020-11-25 08:08:14 +0000

    Initial Commit

---
+++ NameFace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameFace
+//
+//  Created by Paul Houghton on 25/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NameFace/NameFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameFaceApp.swift
+//  NameFace
+//
+//  Created by Paul Houghton on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct NameFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
